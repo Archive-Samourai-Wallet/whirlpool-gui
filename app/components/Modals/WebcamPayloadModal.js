c61a1539c798d0e7b582ed174246708a5d95428c
zeroleak <zeroleak@samourai.io>
2024-01-17 08:44:49 +0100

    fix: camera scanning issues

--- app/components/Modals/WebcamPayloadModal.js
+++ app/components/Modals/WebcamPayloadModal.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { useEffect, useState } from 'react';
-import {QrScanner} from '@yudiel/react-qr-scanner';
+import { QrReader } from 'react-qr-reader';
 import ModalUtils from '../../services/modalUtils';
 import GenericModal from './GenericModal';
 
@@ -17,15 +17,17 @@ export default function WebcamPayloadModal(props) {
         <div className="col-sm-12">
           Get your <strong>Whirlpool pairing payload</strong> from Samourai Wallet. Navigate to: <strong>Settings &gt; Transactions &gt; Pair to Whirlpool GUI</strong>
           <div className="text-center pt-4">
-            <QrScanner
-              onDecode={(result) => {
-                console.log('qr: '+result)
-                onScan(result);
-                onClose();
+            <QrReader
+              onResult={(result, error) => {
+                if (!!result) {
+                  onScan(result?.text);
+                  onClose();
+                }
+                if (!!error) {
+                  modalUtils.setError('Could not read QR code: ' + error.message)
+                }
               }}
-              onError={(error) => {
-                modalUtils.setError('Could not read QR code: ' + error.message)
-              }}
+              style={{ width: '80%' }}
             />
           </div>
         </div>
